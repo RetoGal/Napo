a06f171a1abeb1c8fa3325ad6a6b7ff42410b34f
Reto <91146247+RetoGal@users.noreply.github.com>
2022-05-30 16:00:34 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+const objImg = {
+  rabbit: { name: "rabbit", src: "./img/zayac.png" },
+  wolf: { name: "wolf", src: "./img/volk.png" },
+  ban: { name: "ban", src: "./img/ban.png" },
+  home: { name: "home", src: "./img/home.png" },
+};
+
+function createMatrix() {
+  const select = parseInt(document.querySelector("select").value)
+  const matrix = new Array(select)
+    .fill(null)
+    .map(() => new Array(select).fill(null));
+
+  return matrix;
+}
+
+function setRabbit() {
+  const arr = createMatrix();
+  const rnd = Math.floor(Math.random() * arr.length);
+
+  if (arr[rnd][rnd] === null) {
+    arr[rnd][rnd] = objImg.rabbit.name;
+  } else {
+    setRabbit();
+  }
+
+  return arr;
+}
+
+function setWolf() {
+  const arr = setRabbit();
+  const rnd = Math.floor(Math.random() * arr.length);
+
+  if (arr[rnd][rnd] === null) {
+    arr[rnd][rnd] = objImg.wolf.name;
+  } else {
+    setWolf();
+  }
+
+  return arr;
+}
+
+function setBan() {
+  const arr = setWolf();
+  const rnd = Math.floor(Math.random() * arr.length);
+
+  if (arr[rnd][rnd] === null) {
+    arr[rnd][rnd] = objImg.ban.name;
+  } else {
+    setBan();
+  }
+  return arr
+}
+
+
+
+function setHome() {
+  const arr = setBan();
+  const rnd = Math.floor(Math.random() * arr.length);
+
+  if (arr[rnd][rnd] === null) {
+    arr[rnd][rnd] = objImg.home.name;
+  } else {
+    setHome();
+  }
+  console.log(arr);
+}
+
+
+
+document.querySelector("button").onclick = () => {
+  const select = document.querySelector("select");
+
+  if (select.value == 5) {
+    createMatrix();
+    setRabbit();
+    setWolf();
+    setBan();
+    setHome()
+    
+    
+  }
+};
